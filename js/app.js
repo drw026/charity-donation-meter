6594af5f363b0eead820fa33fb354311d60fac97
Andrew Reasoa <andrew@zen7.nl>
2017-10-24 21:49:11 +0200

    update entry point

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 import '../css/app.scss';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import initialize from './settings';
-import test from './actionCenter';
+import './actionCenter';
+import './actionController';
