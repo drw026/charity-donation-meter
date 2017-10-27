cdf7b1deb9983bf1316d19d72cecb08856a11a82
Andrew Reasoa <andrew@zen7.nl>
2017-10-27 20:02:00 +0200

    add amount functionality

--- js/actionCenter.js
+++ js/actionCenter.js
@@ -12,18 +12,31 @@ const $settingsButton = $actionCenter.find('.settings__button');
  */
 function addAmount() {
 
+    // store input value
+    const amount = $amountInput.val();
+
+    // only submit values
+    if (amount === '' || amount === 0 ) {
+
+        return;
+
+    }
+
     // emit action
-    eev.emit('action-add', $amountInput.val());
+    eev.emit('action-add', amount);
 
     // reset amount input
     $amountInput.val('');
 
 }
 
+// add event to amount button
 $amountButton.on('click', addAmount);
 
+// add event to settings button
 $settingsButton.on('click', function() {
 
+    // emit event to open settings
     eev.emit('settings', { action: 'open' });
 
 });
